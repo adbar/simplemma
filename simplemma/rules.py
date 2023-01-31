5b606a85ea8ece134b875f73ab2677e4149608a6
Juanjo Diaz <juanjo.diazmo@gmail.com>
2023-01-31 16:39:19 +0100

    perf: move english rules to regex

--- simplemma/rules.py
+++ simplemma/rules.py
@@ -156,53 +156,49 @@ def apply_de(token: str, greedy: bool = False) -> Optional[str]:
     return None
 
 
-ENGLISH_IES_ENDING = ["cies", "ries", "ties", "quies"]
-ENGLISH_S_ENDING = ["doms", "isms", "ists", "ments", "nces", "ships", "tions", "ums"]
+ENGLISH_IES_ENDING = re.compile(r"(c|r|t|q)ies")
+ENGLISH_S_ENDING = re.compile(r"(dom|ism|ist|ment|nce|ship|tion|um)s$")
 
 
 def apply_en(token: str) -> Optional[str]:
     "Apply pre-defined rules for English."
     # nouns
-    if token[-1] == "s":
-        if len(token) > 7 and any(
-            token.endswith(ending) for ending in ENGLISH_IES_ENDING
-        ):
-            return token[:-3] + "y"
-        if any(token.endswith(ending) for ending in ENGLISH_S_ENDING):
-            return token[:-1]
-        if token.endswith("esses"):
-            return token[:-2]
-        if token.endswith("trices"):
-            return token[:-3] + "x"
-        # too much noise
-        # if token.endswith("ae"):  # or token.endswith("as")
-        #    return token[:-1]
-        # if token.endswith("oes"):
-        #    return token[:-2]
-        # Pattern rules
-        # if token.endswith("ies") and len(token) > 3 and token[-4] not in VOWELS:
-        #    return token[:-3] + "y"  # complies => comply
-        # if token.endswith(("sses", "shes", "ches", "xes")):
-        #    return token[:-2]  # kisses => kiss
-        # if token.endswith("erves"):
-        #    return token[:-1]
-        # if token.endswith("arves"):
-        #    return token[:-3] + "f"
+    if len(token) > 7 and ENGLISH_IES_ENDING.search(token):
+        return token[:-3] + "y"
+    if ENGLISH_S_ENDING.search(token):
+        return token[:-1]
+    if token.endswith("esses"):
+        return token[:-2]
+    if token.endswith("trices"):
+        return token[:-3] + "x"
+    # too much noise
+    # if token.endswith("ae"):  # or token.endswith("as")
+    #    return token[:-1]
+    # if token.endswith("oes"):
+    #    return token[:-2]
+    # Pattern rules
+    # if token.endswith("ies") and len(token) > 3 and token[-4] not in VOWELS:
+    #    return token[:-3] + "y"  # complies => comply
+    # if token.endswith(("sses", "shes", "ches", "xes")):
+    #    return token[:-2]  # kisses => kiss
+    # if token.endswith("erves"):
+    #    return token[:-1]
+    # if token.endswith("arves"):
+    #    return token[:-3] + "f"
 
-    # verbs
-    # elif token.endswith("ed"):
-    #    if token.endswith("ated"):
-    #        return token[:-4] + "ate"
-    #    if token.endswith("ened"):
-    #        return token[:-4] + "en"
-    #    if token.endswith("fied"):
-    #        return token[:-4] + "fy"
-    #    if token.endswith("ized"):
-    #        return token[:-4] + "ize"
-    #    # Pattern rules
-    #    if token.endswith("ied"):
-    #        return token[:-3] + "y"  # envied => envy
-    #    # -ed could be added
+    # verbs endswith("ed"):
+    #   if token.endswith("ated"):
+    #       return token[:-4] + "ate"
+    #   if token.endswith("ened"):
+    #       return token[:-4] + "en"
+    #   if token.endswith("fied"):
+    #       return token[:-4] + "fy"
+    #   if token.endswith("ized"):
+    #       return token[:-4] + "ize"
+    #   # Pattern rules
+    #   if token.endswith("ied"):
+    #       return token[:-3] + "y"  # envied => envy
+    #   # -ed could be added
     return None
 
 
