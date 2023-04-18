37d6e7ce9257260c579252944b541a38aecc6d93
Adrien Barbaresi <barbaresi@bbaw.de>
2023-04-18 18:33:21 +0200

    fix strict mypy warning

--- simplemma/strategies/defaultprefixes/__init__.py
+++ simplemma/strategies/defaultprefixes/__init__.py
@@ -5,7 +5,7 @@ from typing import Dict, Pattern
 from .de import DE_PREFIX_REGEX
 from .ru import RU_PREFIX_REGEX
 
-DEFAULT_KNOWN_PREFIXES: Dict[str, Pattern] = {
+DEFAULT_KNOWN_PREFIXES: Dict[str, Pattern[str]] = {
     "de": DE_PREFIX_REGEX,
     "ru": RU_PREFIX_REGEX,
 }
