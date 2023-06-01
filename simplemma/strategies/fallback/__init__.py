e216ad5d277b5559e02905c2a860b038def1401f
Juanjo Diaz <juanjo.diazmo@gmail.com>
2023-06-01 17:26:11 +0200

    fix: broken build (#104)

---
+++ simplemma/strategies/fallback/__init__.py
@@ -0,0 +1,5 @@
+"""Simplemma fallback strategies module"""
+
+from .lemmatization_fallback_strategy import LemmatizationFallbackStrategy
+from .to_lowercase import ToLowercaseFallbackStrategy
+from .raise_error import RaiseErrorFallbackStrategy
