e1fb6a07d2896b153b18cacf5bf40803b23115de
Juanjo Diaz <judi@languagewire.com>
2024-08-12 13:45:36 +0200

    fix: cyclic import (#148)

---
+++ simplemma/__metadata__.py
@@ -0,0 +1,18 @@
+"""
+Package Metadata
+
+This module contains metadata information for the simplemma package.
+
+Attributes:
+    __title__ (str): The title of the package.
+    __author__ (str): The authors of the package.
+    __email__ (str): The contact email for the package.
+    __license__ (str): The license under which the package is distributed.
+    __version__ (str): The current version of the package.
+"""
+
+__title__ = "simplemma"
+__author__ = "Adrien Barbaresi, Juanjo Diaz and contributors"
+__email__ = "barbaresi@bbaw.de"
+__license__ = "MIT"
+__version__ = "1.1.1"
