10cc7fb7af8040da86042b5869aeb307226c9010
Adrien Barbaresi <adbar@users.noreply.github.com>
2024-10-09 14:28:47 +0200

    setup: use explicit re-exports (#151)
    * setup: use explicit re-exports
    
    * fix tests

--- simplemma/strategies/dictionaries/__init__.py
+++ simplemma/strategies/dictionaries/__init__.py
@@ -2,3 +2,5 @@
 
 from .dictionary_factory import DefaultDictionaryFactory, DictionaryFactory
 from .trie_directory_factory import TrieDictionaryFactory
+
+__all__ = ["DefaultDictionaryFactory", "DictionaryFactory", "TrieDictionaryFactory"]
